7972e23b972060273a558a7a5b76da67749b655c
Thom Seddon <thom@seddonmedia.co.uk>
2014-07-07 10:03:18 +0100

    Fix Analytics

--- background.js
+++ background.js
@@ -203,4 +203,5 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-17516578-20', 'auto');
+ga('set', 'checkProtocolTask', function(){});
 ga('send', 'pageview');
